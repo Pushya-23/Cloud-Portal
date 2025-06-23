cba938784314b2faaf14db07718306a9a0a51c5e
Pushya-23 <167326530+Pushya-23@users.noreply.github.com>
2025-06-24 01:52:04 +0530

    Updated dashboard components and layout

--- cloud-portal/frontend/components/.ts
+++
@@ -1,6 +0,0 @@
-import { Header } from "../components/Homepage/Header";
-import { IntroText } from "../components/Homepage/Introtext";
-import { FeatureCards } from "../components/Homepage/Featurecard";
-import { FeatureDetail } from "../components/Homepage/Featuredetails";
-import { SocialLinks } from "../components/Homepage/SocialLinks";
-import { FeatureKey } from "../components/Homepage/Featuresdata";
